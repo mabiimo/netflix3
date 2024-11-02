d24c18c17263e0887b13a02510485c72a75a9c87
Muhammad Aryo Bimo <aryo.bimo@mastersystem.co.id>
2024-11-03 00:44:52 +0700

    first commit

---
+++ navbar.php
@@ -0,0 +1,23 @@
+<header id="header" class="header-linear">
+    <div class="header-cont container">
+        <div class="left-cont">
+            <img decoding="async" class="header-brand" src="https://images.ctfassets.net/4cd45et68cgf/7LrExJ6PAj6MSIPkDyCO86/542b1dfabbf3959908f69be546879952/Netflix-Brand-Logo.png" alt="">
+            <ul class="main-nav">
+                <li class="nav-item active">Home</li>
+                <li class="nav-item">TV Shows</li>
+                <li class="nav-item">Movies</li>
+                <li class="nav-item">News & Popular</li>
+                <li class="nav-item">My List</li>
+            </ul>
+        </div>
+        <div class="right-cont">
+            <!-- SVG Icons for Search and Notifications -->
+            <svg width="24" height="24" fill="currentColor" class="search-icon"><path d="M13 11..."></path></svg>
+            <svg width="24" height="24" fill="#fff" class="notification-icon"><path d="M12 4..."></path></svg>
+        </div>
+    </div>
+</header>
+_notes/
+.vscode/
+testing/
+documents/
